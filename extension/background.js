16093348848332b2cac63f787d924e796b94efd2
ISh- <imwaves@gmail.com>
2022-01-08 01:27:37 +0300

    replace all taba with 2 spaces sorry :)

--- extension/background.js
+++ extension/background.js
@@ -1,67 +1,67 @@
 import {
-	Storage,
-	log,
+  Storage,
+  log,
 } from './utils.js';
 
 // drop all tabs settings after last browser boot
 (async function dropStorageTabsKeys () {
-	const items = await Storage.getAll();
+  const items = await Storage.getAll();
   const tabKeys = Object.keys(items)
-  	.filter(key => key.startsWith('tab_'));
-	log('storage tab keys droped', await Storage.remove(tabKeys));
+    .filter(key => key.startsWith('tab_'));
+  log('storage tab keys droped', await Storage.remove(tabKeys));
 })()
 
 chrome.runtime.onMessage.addListener((msg, sender) => {
   const fn = msgs[msg.name];
   if (fn)
-  	fn(msg, sender);
+    fn(msg, sender);
 });
 
 const msgs = {
-	async ['content:init'] ({ frameId }, sender) {
-		const { tab } = sender;
-		// 'all' and 'host' options is not implemented yet
-		// const url = new URL(tab.url);
-		const tabKey = `tab_${ tab.id }`;
-		// const allKey = 'all';
-		// const hostKey = `host_${ url.host }`;
-		const res = await Storage.getSome([ tabKey/*, allKey, hostKey*/ ]);
-		const tabDeviceId = res[tabKey];
-		// const hostDeviceId = res[hostKey];
-		// const allDeviceId = res[allKey];
+  async ['content:init'] ({ frameId }, sender) {
+    const { tab } = sender;
+    // 'all' and 'host' options is not implemented yet
+    // const url = new URL(tab.url);
+    const tabKey = `tab_${ tab.id }`;
+    // const allKey = 'all';
+    // const hostKey = `host_${ url.host }`;
+    const res = await Storage.getSome([ tabKey/*, allKey, hostKey*/ ]);
+    const tabDeviceId = res[tabKey];
+    // const hostDeviceId = res[hostKey];
+    // const allDeviceId = res[allKey];
 
-		const deviceId = /*allDeviceId || hostDeviceId || */tabDeviceId || null;
-		// const target = allDeviceId ? 'all' : hostDeviceId ? 'host' : tabDeviceId ? 'tab' : null;
-		log('content:init', { frameId }, sender, '->', deviceId);
+    const deviceId = /*allDeviceId || hostDeviceId || */tabDeviceId || null;
+    // const target = allDeviceId ? 'all' : hostDeviceId ? 'host' : tabDeviceId ? 'tab' : null;
+    log('content:init', { frameId }, sender, '->', deviceId);
 
     chrome.tabs.sendMessage(tab.id, {
-    	name: 'content:init:resp',
-    	deviceId,
-    	// target,
-    	tabId: tab.id,
-    	frameId,
+      name: 'content:init:resp',
+      deviceId,
+      // target,
+      tabId: tab.id,
+      frameId,
     });
 
     updatePopupIconText({ tabId: tab.id, deviceId });
-	},
+  },
 
-	['updatePopupIconText'] ({ deviceId }, sender) {
-		updatePopupIconText({
-			deviceId,
-			tabId: sender.tab.id,
-		});
-	},
+  ['updatePopupIconText'] ({ deviceId }, sender) {
+    updatePopupIconText({
+      deviceId,
+      tabId: sender.tab.id,
+    });
+  },
 
-	['updatePopupIconTheme'] ({ isDark }) {
-		chrome.browserAction.setIcon({
-	  	path: isDark ? 'Icon128-white.png' : 'Icon128.png',
-	  });
-	}
+  ['updatePopupIconTheme'] ({ isDark }) {
+    chrome.browserAction.setIcon({
+      path: isDark ? 'Icon128-white.png' : 'Icon128.png',
+    });
+  }
 };
 
 function updatePopupIconText ({ tabId, deviceId }) {
-	chrome.browserAction.setBadgeText({
-		tabId,
-		text: (!deviceId || deviceId === 'default') ? '' : 'Ω',
-	});
+  chrome.browserAction.setBadgeText({
+    tabId,
+    text: (!deviceId || deviceId === 'default') ? '' : 'Ω',
+  });
 }
--- extension/content.js
+++ extension/content.js
@@ -2,25 +2,25 @@ let inited = false;
 let currentDeviceId;
 const frameId = Date.now();
 function init (deviceId) {
-	currentDeviceId = deviceId;
-	if (!inited) {
-		inited = true;
-		inject(AuRoPatchContent);
-	}
-	inject(AuRoSetOutputDevice(deviceId))
-	chrome.runtime.sendMessage({
-		name: 'updatePopupIconText',
-		deviceId,
-	});
+  currentDeviceId = deviceId;
+  if (!inited) {
+    inited = true;
+    inject(AuRoPatchContent);
+  }
+  inject(AuRoSetOutputDevice(deviceId))
+  chrome.runtime.sendMessage({
+    name: 'updatePopupIconText',
+    deviceId,
+  });
 }
 
 chrome.runtime.sendMessage({
-	name: 'content:init',
-	frameId,
+  name: 'content:init',
+  frameId,
 });
 
 var frameDepth = (function getDepth(w) {
-	return w.parent === w ? 0 : 1 + getDepth(w.parent);
+  return w.parent === w ? 0 : 1 + getDepth(w.parent);
 })(window);
 
 function filterOutputDevices (devices) {
@@ -35,166 +35,166 @@ function log (desc, ...args) {
 }
 
 function listenStorage (tabId) {
-	chrome.storage.onChanged.addListener(function (changes, namespace) {
-	  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-	    if (key === `tab_${ tabId }`) {
-	    	init(newValue);
-	    }
-	  }
-	});
+  chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+      if (key === `tab_${ tabId }`) {
+        init(newValue);
+      }
+    }
+  });
 }
 
 chrome.runtime.onMessage.addListener(
-	(msg, sender, send) => {
-		log('onMessage', { msg, sender });
-		
-		switch (msg.name) {
-			case 'content:init:resp':
-				if (msg.frameId !== frameId)
-					return;
-				if (msg.deviceId && msg.deviceId !== 'default') {
-					init(msg.deviceId);
-				}
-				listenStorage(msg.tabId);
-				break;
+  (msg, sender, send) => {
+    log('onMessage', { msg, sender });
+    
+    switch (msg.name) {
+      case 'content:init:resp':
+        if (msg.frameId !== frameId)
+          return;
+        if (msg.deviceId && msg.deviceId !== 'default') {
+          init(msg.deviceId);
+        }
+        listenStorage(msg.tabId);
+        break;
 
-			case 'popup:getState':
-				navigator.mediaDevices.getUserMedia({ audio: true }).then(() => {
-					return navigator.mediaDevices.enumerateDevices().then(devices => {
-						chrome.runtime.sendMessage({
-							id: msg.id,
-							name: 'popup:getState:resp',
-							devices: filterOutputDevices(devices),
-							currentDeviceId,
-							frameId,
-						});
-					});
-				}).catch(err => {});
-				break;
+      case 'popup:getState':
+        navigator.mediaDevices.getUserMedia({ audio: true }).then(() => {
+          return navigator.mediaDevices.enumerateDevices().then(devices => {
+            chrome.runtime.sendMessage({
+              id: msg.id,
+              name: 'popup:getState:resp',
+              devices: filterOutputDevices(devices),
+              currentDeviceId,
+              frameId,
+            });
+          });
+        }).catch(err => {});
+        break;
 
-			default:
-				console.error('AuRo :: Unknown msg!!!');
-				log('Unknown msg!')(msg);
-		}
-	}
+      default:
+        console.error('AuRo :: Unknown msg!!!');
+        log('Unknown msg!')(msg);
+    }
+  }
 );
 
 function AuRoSetOutputDevice (deviceId) {
-	return `() => { $AuRo.update('${ deviceId }') }`;
+  return `() => { $AuRo.update('${ deviceId }') }`;
 }
 
 if (frameDepth === 0) {
-	const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-	chrome.runtime.sendMessage({
-		name: 'updatePopupIconTheme',
-		isDark,
-	});
+  chrome.runtime.sendMessage({
+    name: 'updatePopupIconTheme',
+    isDark,
+  });
 }
 
 function AuRoPatchContent () {
-	window.$AuRo = {
-		els: [],
-		addEl (el) {
-			let i = this.els.indexOf(el);
-			if (~i)
-				this.els.splice(i, 1);
-			this.els.unshift(el);
-		},
+  window.$AuRo = {
+    els: [],
+    addEl (el) {
+      let i = this.els.indexOf(el);
+      if (~i)
+        this.els.splice(i, 1);
+      this.els.unshift(el);
+    },
 
-		deviceId: null,
-		update (deviceId) {
-			$AuRo.deviceId = deviceId;
-			l('update()', { deviceId });
+    deviceId: null,
+    update (deviceId) {
+      $AuRo.deviceId = deviceId;
+      l('update()', { deviceId });
 
-			Promise.all(this.els.map(el => {
-				return new Promise(rslv => {
-					const prevTime = el.currentTime;
-					setTimeout(() => {
-						rslv(prevTime !== el.currentTime ? el : null);
-					}, 5);
-				})
-			})).then(elsWhichTimesAreChanging => {
-				const playingEls = elsWhichTimesAreChanging.filter(Boolean);
-				playingEls.forEach(el => {
-					el.pause();
-					el.setSinkId(deviceId);
-					setTimeout(() => el.play(), 3);
-				})
+      Promise.all(this.els.map(el => {
+        return new Promise(rslv => {
+          const prevTime = el.currentTime;
+          setTimeout(() => {
+            rslv(prevTime !== el.currentTime ? el : null);
+          }, 5);
+        })
+      })).then(elsWhichTimesAreChanging => {
+        const playingEls = elsWhichTimesAreChanging.filter(Boolean);
+        playingEls.forEach(el => {
+          el.pause();
+          el.setSinkId(deviceId);
+          setTimeout(() => el.play(), 3);
+        })
 
-				l('update().setSindId() for: ', playingEls);
-			});
-		}
-	};
+        l('update().setSindId() for: ', playingEls);
+      });
+    }
+  };
 
-	const getDeviceId = () => navigator.mediaDevices.getUserMedia({ audio: true }).then(() => {
-		return navigator.mediaDevices.enumerateDevices().then(devices => {
-			l({devices})
-			const selectedDevice = devices.find(({ deviceId }) => deviceId === $AuRo.deviceId);
-			if (selectedDevice)
-				return selectedDevice.deviceId;
-			return Promise.reject('deviceId ' + $AuRo.deviceId + ' not found');
-		});
-	});
+  const getDeviceId = () => navigator.mediaDevices.getUserMedia({ audio: true }).then(() => {
+    return navigator.mediaDevices.enumerateDevices().then(devices => {
+      l({devices})
+      const selectedDevice = devices.find(({ deviceId }) => deviceId === $AuRo.deviceId);
+      if (selectedDevice)
+        return selectedDevice.deviceId;
+      return Promise.reject('deviceId ' + $AuRo.deviceId + ' not found');
+    });
+  });
 
-	var frameDepth = (function getDepth(w) {
-		return w.parent === w ? 0 : 1 + getDepth(w.parent);
-	})(window);
+  var frameDepth = (function getDepth(w) {
+    return w.parent === w ? 0 : 1 + getDepth(w.parent);
+  })(window);
 
-	var _audioPlay = HTMLAudioElement.prototype.play;
-	HTMLAudioElement.prototype.play = function () {
-		l('Audio.play()', this);
-		$AuRo.addEl(this);
-		getDeviceId().then(deviceId => {
-			this.setSinkId($AuRo.deviceId).then(arg => {
-				l('Audio.play().getDeviceId().setSinkId()', { deviceId });
-				_audioPlay.call(this); 
-			});
-		}).catch(err => {
-			_audioPlay.call(this);
-			le(err);
-		});
-	};
+  var _audioPlay = HTMLAudioElement.prototype.play;
+  HTMLAudioElement.prototype.play = function () {
+    l('Audio.play()', this);
+    $AuRo.addEl(this);
+    getDeviceId().then(deviceId => {
+      this.setSinkId($AuRo.deviceId).then(arg => {
+        l('Audio.play().getDeviceId().setSinkId()', { deviceId });
+        _audioPlay.call(this); 
+      });
+    }).catch(err => {
+      _audioPlay.call(this);
+      le(err);
+    });
+  };
 
-	var _videoPlay = HTMLVideoElement.prototype.play;
-	HTMLVideoElement.prototype.play = function () {
-		l('Video.play()', this);
-		$AuRo.addEl(this);
-		getDeviceId().then(deviceId => {
-			this.setSinkId($AuRo.deviceId).then(arg => {
-				l('Video.play().getDeviceId().setSinkId()', { deviceId });
-				_videoPlay.call(this); 
-			});
-		}).catch(err => {
-			_videoPlay.call(this);
-			le(err);
-		});
-	};
+  var _videoPlay = HTMLVideoElement.prototype.play;
+  HTMLVideoElement.prototype.play = function () {
+    l('Video.play()', this);
+    $AuRo.addEl(this);
+    getDeviceId().then(deviceId => {
+      this.setSinkId($AuRo.deviceId).then(arg => {
+        l('Video.play().getDeviceId().setSinkId()', { deviceId });
+        _videoPlay.call(this); 
+      });
+    }).catch(err => {
+      _videoPlay.call(this);
+      le(err);
+    });
+  };
 
-	var _aPlay = Audio.prototype.play;
-	Audio.prototype.play = function () {
-		l('Video.play()', this);
-		$AuRo.addEl(this);
-		getDeviceId().then(deviceId => {
-			this.setSinkId($AuRo.deviceId).then(arg => {
-				l('Video.play().getDeviceId().setSinkId()', { deviceId });
-				_aPlay.call(this); 
-			});
-		}).catch(err => {
-			_aPlay.call(this);
-			le(err);
-		});
-	};
+  var _aPlay = Audio.prototype.play;
+  Audio.prototype.play = function () {
+    l('Video.play()', this);
+    $AuRo.addEl(this);
+    getDeviceId().then(deviceId => {
+      this.setSinkId($AuRo.deviceId).then(arg => {
+        l('Video.play().getDeviceId().setSinkId()', { deviceId });
+        _aPlay.call(this); 
+      });
+    }).catch(err => {
+      _aPlay.call(this);
+      le(err);
+    });
+  };
 
-	function l (...args) {
-		console.log('$AuRo (' + frameDepth + '): ', ...args);
-	}
-	function le (...args) {
-		console.error('$AuRo error!')
-		console.log('$AuRo error (' + frameDepth + '): ', ...args);
-	}
+  function l (...args) {
+    console.log('$AuRo (' + frameDepth + '): ', ...args);
+  }
+  function le (...args) {
+    console.error('$AuRo error!')
+    console.log('$AuRo error (' + frameDepth + '): ', ...args);
+  }
 
-	l('Monkeypatched');
+  l('Monkeypatched');
 }
 
 function inject(fn) {
@@ -205,9 +205,9 @@ function inject(fn) {
 }
 
 function l (...args) {
-	console.log('$AuRo: ', ...args);
+  console.log('$AuRo: ', ...args);
 }
 
 function onError (err) {
-	l('Error: ', err);
+  l('Error: ', err);
 }
